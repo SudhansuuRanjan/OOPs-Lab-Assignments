656b3705b09aede7435ae6fe6e39daa3f18810ea
SudhanshuuRanjan <sudhanshur.ug20.ee@nitp.ac.in>
2022-03-05 19:06:17 +0530

    fixed bug

--- Lab 1/src/com/assignment2/question5.java
+++ Lab 1/src/com/assignment2/question5.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class question5 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter the rows & column of first matrix : ");
+        System.out.print("Enter the number of rows & column of matrices : ");
         int m = in.nextInt();
         int n = in.nextInt();
         int[][] arr1 = new int[m][n];
@@ -27,6 +27,18 @@ public class question5 {
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 System.out.print(arr1[i][j] + arr2[i][j] + " ");
+            }
+            System.out.print("\n");
+        }
+
+        System.out.println("Product of Matrices : ");
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int sum = 0;
+                for (int k = 0; k < n; k++) {
+                   sum += arr1[i][k] * arr2[k][j];
+                }
+                System.out.print(sum + " ");
             }
             System.out.print("\n");
         }
